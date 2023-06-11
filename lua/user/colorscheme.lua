489ee33e237e983c2314e0603007ed03ab6c83a2
Anjir Hossain <admin@figtreez.com>
2023-06-11 14:25:06 -0700

    pin

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -3,7 +3,7 @@ local status_ok, vim = pcall(require, "vim")
 if not status_ok then
 	return
 end
-status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+status_ok, vim = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
 	return
 end
